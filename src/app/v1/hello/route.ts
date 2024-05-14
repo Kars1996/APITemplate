c9760d12e967fa787c5ec87a94e7d5c152edc46f
Kars1996 <79771285+Kars1996@users.noreply.github.com>
2024-05-14 19:46:30 +0100

    chore(example): Conditional Params

--- src/app/v1/hello/route.ts
+++ src/app/v1/hello/route.ts
@@ -1,3 +1,5 @@
+import { type NextRequest } from "next/server";
+
 /*
 Copyright © 2024 Kars (github.com/kars1996)
 
@@ -6,16 +8,28 @@ Contact Kars for any enquieries
 */
 
 interface ResponseProp {
-    response: any,
-    status?: number
+    response: any;
+    status?: number;
 }
 
 const Data: ResponseProp = {
     response: "Hi",
-    status: 200
-}
+    status: 200,
+};
 
-export async function GET(): Promise<Response> {
+export function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get("key");
+    if (query?.toLowerCase() === "kars") {
+        return new Response(
+            JSON.stringify({ response: "How cool well done :D", status: 200 }),
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+    }
     return new Response(JSON.stringify(Data), {
         headers: {
             "Content-Type": "application/json",
